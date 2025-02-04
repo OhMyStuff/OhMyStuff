4963d32d39c235119038254185544f75974f3d30
zggln2rk <zggln2rk@outlook.com>
2025-02-04 22:32:49 +0800

    feat(layout): Complete the settings page layout

---
+++ lib/screens/settings.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.large(
+            title: Text('设置'),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                ListTile(
+                  leading: Icon(Icons.color_lens_rounded),
+                  title: Text('自定义'),
+                  subtitle: Text('深色模式、动态取色、语言'),
+                  onTap: () => context.go('/settings/customize'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.info_rounded),
+                  title: Text('关于'),
+                  subtitle: Text('版本、意见反馈、自动更新'),
+                  onTap: () => context.go('/settings/about'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
