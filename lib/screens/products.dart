b4f7cfd49c66e75c0b8d896f7b13ca98b5b6a76e
zggln2rk <zggln2rk@outlook.com>
2025-02-23 11:59:57 +0800

    feat(layout): Complete the tab bar layout

---
+++ lib/screens/products.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/custom_tile.dart';
+
+class ProductsPage extends StatelessWidget {
+  const ProductsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        CustomTile(
+          title: 'Soda',
+          subtitle: [],
+          onTap: () {},
+        ),
+        CustomTile(
+          title: 'Cookies',
+          subtitle: [],
+          onTap: () {},
+        ),
+        CustomTile(
+          title: 'Chocolate',
+          subtitle: [],
+          onTap: () {},
+        ),
+        CustomTile(
+          title: 'Eggs',
+          subtitle: [],
+          onTap: () {},
+        ),
+        CustomTile(
+          title: 'Yogurt',
+          subtitle: [],
+          onTap: () {},
+        ),
+        CustomTile(
+          title: 'Noodles',
+          subtitle: [],
+          onTap: () {},
+        ),
+        CustomTile(
+          title: 'Cheese',
+          subtitle: [],
+          onTap: () {},
+        ),
+        CustomTile(
+          title: 'Cucumber',
+          subtitle: [],
+          onTap: () {},
+        ),
+        CustomTile(
+          title: 'Tomato',
+          subtitle: [],
+          onTap: () {},
+        ),
+        CustomTile(
+          title: 'Milk',
+          subtitle: [],
+          onTap: () {},
+        ),
+      ],
+    );
+  }
+}
