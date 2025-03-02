bfcdbf4c9e74cec79b6a8f519920cce694ebbade
zggln2rk <zggln2rk@outlook.com>
2025-03-02 21:37:22 +0800

    feat(theme): Enable edge-to-edge display

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'router.dart';
 import 'theme.dart';
 
 void main() {
   runApp(const MainApp());
+
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+      statusBarColor: Colors.transparent,
+    ),
+  );
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
 }
 
 class MainApp extends StatelessWidget {
