03ad4392b61623ec6e9a51077a272a2a9a7f7e4b
zggln2rk <zggln2rk@outlook.com>
2025-04-05 21:44:06 +0800

    feat(layout): Complete the shopping list page layout

--- lib/screens/shopping.dart
+++ lib/screens/shopping.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/shopping_tile.dart';
+
 class ShoppingPage extends StatelessWidget {
   const ShoppingPage({super.key});
 
@@ -8,6 +10,19 @@ class ShoppingPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Shopping list'),
+      ),
+      body: ListView(
+        children: [
+          ShoppingTile(
+            title: 'Soda',
+          ),
+          ShoppingTile(
+            title: 'Eggs',
+          ),
+          ShoppingTile(
+            title: 'Milk',
+          ),
+        ],
       ),
     );
   }
