ac76925a2f41f82648c7aa9dbd4568ee7077226f
zggln2rk <zggln2rk@outlook.com>
2025-04-19 11:07:25 +0800

    feat(widget): Add checked style to ShoppingTile

--- lib/widgets/shopping_tile.dart
+++ lib/widgets/shopping_tile.dart
@@ -4,16 +4,23 @@ class ShoppingTile extends StatelessWidget {
   const ShoppingTile({
     super.key,
     required this.title,
+    this.isChecked = false,
     this.onTap,
   });
 
   final String title;
+  final bool isChecked;
   final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(title),
+      title: Text(
+        title,
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
       onTap: onTap,
     );
   }
