3bcce621c4eec4d10e6e96677a1e0909946fe18c
beztneon <beztneon@outlook.com>
2025-05-31 20:48:28 +0800

    ✨ feat(widget): Add app bar to action pages

--- lib/screens/consume.dart
+++ lib/screens/consume.dart
@@ -5,52 +5,67 @@ class ConsumePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              icon: Icon(Icons.widgets_rounded),
-              labelText: '物品',
-              suffixIcon: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.edit_outlined),
-              ),
-              filled: true,
-            ),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.large(
+            title: Text('Consume'),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              icon: Icon(Icons.category_rounded),
-              labelText: '数量',
-              suffixIcon: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.keyboard_arrow_up_rounded),
+          SliverList.list(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: TextField(
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.widgets_rounded),
+                    labelText: '物品',
+                    suffixIcon: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.edit_outlined),
+                    ),
+                    filled: true,
                   ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.keyboard_arrow_down_rounded),
-                  ),
-                ],
+                ),
               ),
-              filled: true,
-            ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: TextField(
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.category_rounded),
+                    labelText: '数量',
+                    suffixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.keyboard_arrow_up_rounded),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.keyboard_arrow_down_rounded),
+                        ),
+                      ],
+                    ),
+                    filled: true,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.location_on_rounded),
+                title: Text('位置'),
+                subtitle: Text('未选择'),
+                onTap: () {},
+              ),
+            ],
           ),
-        ),
-        ListTile(
-          leading: Icon(Icons.location_on_rounded),
-          title: Text('位置'),
-          subtitle: Text('未选择'),
-          onTap: () {},
-        ),
-      ],
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.arrow_outward_rounded),
+      ),
     );
   }
 }
