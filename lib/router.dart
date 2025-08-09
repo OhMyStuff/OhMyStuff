87ba251aafde80a3eab01cfc2ac017e230a8f51d
beztneon <beztneon@outlook.com>
2025-08-09 17:35:31 +0800

    ✨ feat(layout): Complete the home page layout

--- lib/router.dart
+++ lib/router.dart
@@ -1,22 +1,17 @@
 import 'package:go_router/go_router.dart';
 
 import 'screens/about.dart';
-import 'screens/consume.dart';
-import 'screens/customize.dart';
-import 'screens/inventory.dart';
+import 'screens/home.dart';
 import 'screens/journal.dart';
-import 'screens/locations.dart';
-import 'screens/overview.dart';
-import 'screens/product.dart';
-import 'screens/products.dart';
+import 'screens/stocks.dart';
 import 'screens/purchase.dart';
+import 'screens/customize.dart';
+import 'screens/product.dart';
 import 'screens/settings.dart';
 import 'screens/shopping.dart';
-import 'screens/transfer.dart';
-import 'widgets/custom_navigation.dart';
 
 final router = GoRouter(
-  initialLocation: '/overview',
+  initialLocation: '/',
   redirect: (context, state) {
     if (state.uri.hasScheme) {
       return state.uri.path;
@@ -24,115 +19,45 @@ final router = GoRouter(
     return null;
   },
   routes: [
-    StatefulShellRoute(
-      builder: (context, state, navigationShell) {
-        return navigationShell;
-      },
-      navigatorContainerBuilder: (context, navigationShell, children) {
-        return CustomNavigation(
-          navigationShell: navigationShell,
-          children: children,
-        );
-      },
-      branches: [
-        StatefulShellBranch(
-          preload: true,
-          routes: [
-            GoRoute(
-              path: '/overview',
-              builder: (_, __) => OverviewPage(),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          preload: true,
-          routes: [
-            GoRoute(
-              path: '/stocks',
-              builder: (_, __) => ProductsPage(),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          preload: true,
-          routes: [
-            GoRoute(
-              path: '/shopping',
-              builder: (_, __) => ShoppingPage(),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          preload: true,
-          routes: [
-            GoRoute(
-              path: '/locations',
-              builder: (_, __) => LocationsPage(),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          preload: true,
-          routes: [
-            GoRoute(
-              path: '/purchase',
-              builder: (_, __) => PurchasePage(),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          preload: true,
-          routes: [
-            GoRoute(
-              path: '/consume',
-              builder: (_, __) => ConsumePage(),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          preload: true,
-          routes: [
-            GoRoute(
-              path: '/transfer',
-              builder: (_, __) => TransferPage(),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          preload: true,
-          routes: [
-            GoRoute(
-              path: '/inventory',
-              builder: (_, __) => InventoryPage(),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          preload: true,
-          routes: [
-            GoRoute(
-              path: '/journal',
-              builder: (_, __) => JournalPage(),
-            ),
-          ],
-        ),
-      ],
-    ),
     GoRoute(
-      path: '/product',
-      builder: (_, __) => ProductPage(),
-    ),
-    GoRoute(
-      path: '/settings',
-      builder: (_, __) => SettingsPage(),
+      path: '/',
+      builder: (_, __) => HomePage(),
       routes: [
         GoRoute(
-          path: 'about',
-          builder: (_, __) => AboutPage(),
+          path: 'stocks',
+          builder: (_, __) => StocksPage(),
+          routes: [
+            GoRoute(
+              path: 'soda',
+              builder: (_, __) => ProductPage(),
+            ),
+          ],
         ),
         GoRoute(
-          path: 'customize',
-          builder: (_, __) => CustomizePage(),
+          path: 'shopping',
+          builder: (_, __) => ShoppingPage(),
+        ),
+        GoRoute(
+          path: 'purchase',
+          builder: (_, __) => PurchasePage(),
+        ),
+        GoRoute(
+          path: 'journal',
+          builder: (_, __) => JournalPage(),
+        ),
+        GoRoute(
+          path: 'settings',
+          builder: (_, __) => SettingsPage(),
+          routes: [
+            GoRoute(
+              path: 'about',
+              builder: (_, __) => AboutPage(),
+            ),
+            GoRoute(
+              path: 'customize',
+              builder: (_, __) => CustomizePage(),
+            ),
+          ],
         ),
       ],
     ),
---
+++ lib/screens/stocks.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../widgets/custom_tile.dart';
+
+class StocksPage extends StatelessWidget {
+  const StocksPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.large(
+            title: Text('库存总览'),
+          ),
+          SliverList.list(
+            children: [
+              CustomTile(
+                title: 'Soda',
+                subtitle: [],
+                onTap: () => context.push('/stocks/soda'),
+              ),
+              CustomTile(
+                title: 'Cookies',
+                subtitle: [],
+                onTap: () {},
+              ),
+              CustomTile(
+                title: 'Chocolate',
+                subtitle: [],
+                onTap: () {},
+              ),
+              CustomTile(
+                title: 'Eggs',
+                subtitle: [],
+                onTap: () {},
+              ),
+              CustomTile(
+                title: 'Yogurt',
+                subtitle: [],
+                onTap: () {},
+              ),
+              CustomTile(
+                title: 'Noodles',
+                subtitle: [],
+                onTap: () {},
+              ),
+              CustomTile(
+                title: 'Cheese',
+                subtitle: [],
+                onTap: () {},
+              ),
+              CustomTile(
+                title: 'Cucumber',
+                subtitle: [],
+                onTap: () {},
+              ),
+              CustomTile(
+                title: 'Tomato',
+                subtitle: [],
+                onTap: () {},
+              ),
+              CustomTile(
+                title: 'Milk',
+                subtitle: [],
+                onTap: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search_rounded),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
